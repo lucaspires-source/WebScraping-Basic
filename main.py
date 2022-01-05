b088e1d7a8666f19a9c364950a45b8c37bdf2943
lucaspires-source <pires.lucas94@gmail.com>
2022-01-05 16:31:01 -0300

    Final Touches

--- main.py
+++ main.py
@@ -27,5 +27,5 @@ if __name__ == '__main__':
     while True:
         find_jobs()
         time_wait = 10
-        print(f'Waiting { time_wait} seconds...')
+        print(f'Waiting { time_wait} minutes...')
         time.sleep(time_wait * 60)
