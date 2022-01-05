b878c9adb57487b501cdb5d59ba6f09cb3358f61
lucaspires-source <pires.lucas94@gmail.com>
2022-01-05 16:24:12 -0300

    Filtering out job by skills

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from bs4 import BeautifulSoup
 import requests
 
-
+print('Put some skill that you are not familiar with')
+unfamiliar_skil = input('>')
+print(f'Filtering out {unfamiliar_skil}')
 html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
 soup = BeautifulSoup(html_text, 'lxml')
 jobs = soup.find_all('li', class_= 'clearfix job-bx wht-shd-bx')
@@ -11,6 +13,9 @@ for job in jobs:
         company_name = job.find('h3', class_='joblist-comp-name').text.replace(' ','')
         skills = job.find('span', class_ = 'srp-skills').text.replace(' ','')
         more_info = job.header.h2.a['href']
-        print(f"Company Name: {company_name.strip()}")
-        print(f"Required Skills: {skills.strip()}")
-        print(f"More Info: {more_info.strip()}")
+        if unfamiliar_skil not in skills:
+            print(f"Company Name: {company_name.strip()}")
+            print(f"Required Skills: {skills.strip()}")
+            print(f"More Info: {more_info.strip()}")
+
+            print(' ')
