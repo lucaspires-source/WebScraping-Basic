406b1c6eafd3aab95226a06f8430ff64ac87d839
lucaspires-source <pires.lucas94@gmail.com>
2022-01-05 15:48:39 -0300

    Looping through similar soup.find_all() objects

--- main.py
+++ main.py
@@ -4,13 +4,16 @@ import requests
 
 html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
 soup = BeautifulSoup(html_text, 'lxml')
-job = soup.find('li', class_= 'clearfix job-bx wht-shd-bx')
-company_name = job.find('h3', class_='joblist-comp-name').text.replace(' ','')
-skills = job.find('span', class_ = 'srp-skills').text.replace(' ','')
-published_date = job.find('span', class_='sim-posted').span.text
-print(f'''
-Company Name: {company_name}
-Required Skills: {skills}
-Published Date:{published_date}
-'''
-)
+jobs = soup.find_all('li', class_= 'clearfix job-bx wht-shd-bx')
+for job in jobs:
+    published_date = job.find('span', class_='sim-posted').span.text
+    if 'few' in published_date:
+        company_name = job.find('h3', class_='joblist-comp-name').text.replace(' ','')
+        skills = job.find('span', class_ = 'srp-skills').text.replace(' ','')
+        
+        print(f'''
+        Company Name: {company_name}
+        Required Skills: {skills}
+        '''
+    
+    )
