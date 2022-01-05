ebdec38672a745228043bfa930142f4798696c93
lucaspires-source <pires.lucas94@gmail.com>
2022-01-05 15:41:56 -0300

    Scraping a Production Website

--- main.py
+++ main.py
@@ -3,5 +3,14 @@ import requests
 
 
 html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
-
-print(html_text)
+soup = BeautifulSoup(html_text, 'lxml')
+job = soup.find('li', class_= 'clearfix job-bx wht-shd-bx')
+company_name = job.find('h3', class_='joblist-comp-name').text.replace(' ','')
+skills = job.find('span', class_ = 'srp-skills').text.replace(' ','')
+published_date = job.find('span', class_='sim-posted').span.text
+print(f'''
+Company Name: {company_name}
+Required Skills: {skills}
+Published Date:{published_date}
+'''
+)
